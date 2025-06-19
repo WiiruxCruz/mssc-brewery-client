8e04ff8e413bf6ea84d07ff72424a1a34b654851
WiiruxCruz <hialf.92@gmail.com>
2025-06-18 23:55:42 -0600

    adding post client

--- src/main/java/guru/springframework/mssc_brewery_client/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/mssc_brewery_client/web/client/BreweryClient.java
@@ -1,5 +1,6 @@
 package guru.springframework.mssc_brewery_client.web.client;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -27,5 +28,9 @@ public class BreweryClient {
 	
 	public BeerDto getBeerById(UUID uuid) {
 		return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+	}
+	
+	public URI saveNewBeer(BeerDto beerDto) {
+		return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
 	}
 }
--- src/test/java/guru/springframework/mssc_brewery_client/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/mssc_brewery_client/web/client/BreweryClientTest.java
@@ -2,6 +2,7 @@ package guru.springframework.mssc_brewery_client.web.client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,16 @@ class BreweryClientTest {
 	void testGetBeerById() {
 		BeerDto dto = client.getBeerById(UUID.randomUUID());
 		assertNotNull(dto);
+	}
+	
+	@Test
+	void testSavedNewBeer() {
+		BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+		
+		URI url = client.saveNewBeer(beerDto);
+		
+		assertNotNull(url);
+		
+		System.out.println(url.toString());
 	}
 }
